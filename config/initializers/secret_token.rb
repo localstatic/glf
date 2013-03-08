b6334b0154b54e886eae95696013611c9b42b3a3
Morgan Terry <localstatic@localstatic.com>
2013-03-07 21:43:17 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glf::Application.config.secret_token = 'e13c7eb734fec581bc62fa7836b5ce2f5410ea49d112fd740ae5fe379fe478c8d64c2959460d1b83238e18f8d084f756573111328e2d77833b1077e86a92f4f6'
