5211373501d8d260359e4f8d10550ebdb87f1baf
Morgan Terry <localstatic@localstatic.com>
2013-04-14 14:48:43 -0600

    Disable "pending" test for now

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+# TODO: Write some tests
+#  pending "add some examples to (or delete) #{__FILE__}"
 end
