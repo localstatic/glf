30db1bb40c04e811391f486c618b80dba60e0132
Morgan Terry <localstatic@localstatic.com>
2013-05-07 21:51:25 -0600

    Put website link for places next to the name instead of making the name itself a link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,12 +8,19 @@ module ApplicationHelper
     end
   end
 
-  def place_name_link(place)
-    if place.url.empty?
-      place.name
-    else
-      link_to place.name, place.url, target: "_blank"
+  def place_name_link(place, options = nil)
+    options = {} if options.nil?
+    str = place.name
+
+    unless place.url.empty?
+      if options[:name_as_link]
+        str = link_to str, place.url, target: "_blank"
+      else
+        str += " (#{link_to "website", place.url, target: "_blank"})"
+      end
     end
+
+    raw str
   end
 
 end
