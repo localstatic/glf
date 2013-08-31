7f4bd6af0596419f7add1310f30baed645e0a548
Morgan Terry <localstatic@localstatic.com>
2013-08-30 23:31:33 -0600

    Use consistent place name UI on nominations and places pages. Fixes #43

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,16 +8,26 @@ module ApplicationHelper
     end
   end
 
-  def place_name_link(place, options = nil)
+  def place_name(place, options = nil)
     options = {} if options.nil?
     str = place.name
 
+    place.url.strip!
     unless place.url.empty?
       if options[:name_as_link]
         str = link_to str, place.url, target: "_blank"
       else
         str += " (#{link_to "website", place.url, target: "_blank"})"
       end
+    end
+
+    if place.disabled?
+      str += ' <span class="text-warning">[DISABLED]</span>'
+    end
+
+    if place.description.present?
+      omission = "<span title=\"#{place.description}\"> ...</span>"
+      str += "<span class=\"description\"> - #{place.description.truncate(50 + omission.length - place.description.length, separator: " ", omission: omission)}"
     end
 
     raw str
